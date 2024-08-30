63ac8e5d4ea07872c2868b0228e1f493434ebae3
Umamaheswari22M <ambijayamani2203@gmail.com>
2024-08-30 14:48:16 +0530

    Add files via upload

---
+++ var total.js
@@ -0,0 +1,11 @@
+let name ="Umamaheswari";
+let sub1 = 90;
+let sub2 = 97;
+let sub3 = 89;
+let sub4 = 95;
+let sub5 = 99;
+let total = sub1 + sub2 + sub3 + sub4 + sub5;
+let average = total/5;
+console.log("Name",name);
+console.log("Total",total);
+console.log("average",average);
