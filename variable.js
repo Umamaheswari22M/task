ef1e6e8ee6f27239ba6b866c2bda5ac920028bf2
Umamaheswari22M <ambijayamani2203@gmail.com>
2024-08-30 14:47:49 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,12 @@
+//Variables
+let age = 20;
+const name ="uma";
+var isStudent = true;
+let person ={
+    firstName: "uma",
+    lastName:"maheswari",
+    age: 20,
+    isStudent: true
+};
+console.log(name,age,isStudent);
+console.log(person);
