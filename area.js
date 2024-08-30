dc8af28257dcd74d3e35b49d739aa3ab584a1b79
Umamaheswari22M <ambijayamani2203@gmail.com>
2024-08-30 14:46:50 +0530

    Add files via upload

---
+++ area.js
@@ -0,0 +1,15 @@
+const PI=3.14;
+let r =5;
+let areaofcircle =PI*r*r;
+console.log("area of circle is:",areaofcircle);
+ let b=10;
+ let h=12;
+ let areaoftraiangle=1/2*b*h;
+ console.log("area of taiangle is :",areaoftraiangle);
+let s=5;
+let areaofsquare =s*s;
+console.log("area of square is:",areaofsquare);
+let t=5;
+let areaofcube=6*t;
+console.log("area of cube is:",areaofcube);
+
