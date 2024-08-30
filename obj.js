de3b3643661957d12d711bdd9f5a966cab5ad452
Umamaheswari22M <ambijayamani2203@gmail.com>
2024-08-30 14:48:47 +0530

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,14 @@
+const bike = {
+    brand:"R15",
+    model:"YAMAHA",
+    special:"immense power",
+    bestversion:"YAMAHA R15V4",
+    mileage: 55.20,
+    Start :function(){
+        console.log("The bike has started.");
+   }
+};
+console.log(bike.brand);
+console.log(bike.model);
+console.log(bike.bestversion);
+bike.start();
